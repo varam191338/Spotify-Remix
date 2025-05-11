f26aac96cc8951e1a1ab95f42980f24fff8fe6d4
varalakshmi <varalakshmikudaka@gmail.com>
2025-05-11 12:03:19 +0000

    solution

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -3,7 +3,36 @@ import {Component} from 'react'
 import './index.css'
 
 class Login extends Component {
+  state = {username: '', password: ''}
+
+  onChangeUsername = event => {
+    this.setState({username: event.target.value})
+  }
+
+  onChangePassword = event => {
+    this.setState({password: event.target.value})
+  }
+
+  onSubmitLogin = async event => {
+    event.preventDefault()
+    const {username, password} = this.state
+    const userDetails = {username, password}
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+    const response = await fetch('https://apis.ccbp.in/login', options)
+    const data = await response.json()
+
+    if (response.ok === true) {
+      this.onSubmitSuccess()
+    } else {
+      this.onSubmitFailure()
+    }
+  }
+
   render() {
+    const {username, password} = this.state
     return (
       <div className="login-container">
         <div className="login-card-container">
@@ -15,15 +44,29 @@ class Login extends Component {
                 className="logo"
               />
             </div>
-            <form className="form-container">
+            <form className="form-container" onSubmit={this.onSubmitLogin}>
               <label htmlFor="username" className="label">
                 USERNAME
               </label>
-              <input type="text" className="input-field" />
+              <input
+                type="text"
+                className="input-field"
+                id="username"
+                value={username}
+                onChange={this.onChangeUsername}
+                autoComplete="username"
+              />
               <label htmlFor="password" className="label">
                 PASSWORD
               </label>
-              <input type="password" className="input-field" />
+              <input
+                type="password"
+                className="input-field"
+                id="password"
+                onChange={this.onChangePassword}
+                value={password}
+                autoComplete="password"
+              />
               <button type="submit" className="login-button">
                 LOGIN
               </button>
